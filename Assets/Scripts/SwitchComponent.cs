810552552c7997876b206fcfb7d778b2ac3649f4
NickABoen <NickABoen@gmail.com>
2016-09-24 08:51:50 -0500

    Add switch prefab and enable it to toggle with a small script

---
+++ Assets/Scripts/SwitchComponent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchComponent : MonoBehaviour {
+
+    public bool isOn = false;
+    public Sprite OnSprite;
+    public Sprite OffSprite;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();
+        renderer.sprite = isOn ? OnSprite : OffSprite;
+	}
+}
