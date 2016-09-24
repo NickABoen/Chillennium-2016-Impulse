059e6f75a60edd49725e7502129426fe21495366
NickABoen <NickABoen@gmail.com>
2016-09-24 13:13:42 -0500

    Test instantiation and wonder why it doesn't work in the compulsion system

---
+++ Assets/Scripts/TestInstantiateSystem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestInstantiateSystem : MonoBehaviour {
+
+    public GameObject prefab;
+	// Use this for initialization
+	void Start () {
+        GameObject.Instantiate(prefab);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
